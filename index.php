b58ebfa9d31d727abaeec1e635bb0dd03a428b6b
kaile wang <s3543953@student.rmit.edu.au>
2019-01-23 15:13:55 +1100

    first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+include('../a2/head.inc');
+include('../a2/nav.inc');
+?>
+<body>
+    <title>Train tracker</title>
+    <main>
+      <div class="main_box">
+	  <div class="search-container">
+	<form action="/index.php">
+      <input type="text" placeholder="Search.." name="search">
+      <button type="submit" ><img src= "search.png"></button>
+    </form>
+  </div>
+<?php
+	//if (no search bar variable has sent)
+	//then: display nothing.
+	//else:
+	//for i to number_of_station:
+       $station_name = "Station name";
+	   echo '<div class="text_box">';
+       echo  "<h2 >$station_name</h2></div>";
+       
+?>
+    </div>
+    </main>
+<?php
+include('../a2/footer.inc');
+?>
+  </body>  
+
+</html>
