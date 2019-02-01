d61af4ac88c65d2e3d758661a9708536ff32bf46
kaile wang <s3543953@student.rmit.edu.au>
2019-02-01 17:29:13 +1100

    improve check function

--- favorite_function.php
+++ favorite_function.php
@@ -14,7 +14,7 @@ $projID= "cc-2019-lab4";
 $datastore = new DatastoreClient(['projectId' => $projID]);
 
 // hardcoded data:
-$search_data = "Richmond Station";
+$search_data = "East Richmond Station";
 echo "search data: $search_data".'<br>';
 //account login and log out
 if (isset($user)) {
@@ -22,14 +22,14 @@ if (isset($user)) {
 		$user->getNickname(),
 		UserService::createLogoutUrl('/'));
 		//add_fav($datastore, $search_data,$user);
-		if (isset($_POST["station"])){
-			echo "<br> value of post:".$_POST['station'];
+		if (isset($_GET["station"])){
+			//echo "<br> value of post:".$_GET['station'];
+			
 			add_fav($datastore, $search_data,$user);
-			echo '<br> operation started';
 		}
 		// favorite button
 		echo 'favorite button:';
-		echo "<form action='/' metnod ='POST'>";
+		echo "<form action='/' metnod ='GET'>";
 		echo '<input type="hidden" name ="station"'. "value = '$search_data' />";
 		echo "<input type = 'submit'/></form>";
 		//print_r($_POST['station']);
@@ -41,13 +41,47 @@ if (isset($user)) {
 }
 
 /*
-upload data to data store
+function: upload data to data store
+@param: DatastoreClient datastore - the datastore we have created.
+		$search_data - the input data comes from search bar.
+		User $user - the object of logged in user
+@return: nothing
 */
 function add_fav(DatastoreClient $datastore, $search_data,User $user)
 {
 $key = $datastore->key('station');
 $task_favorite = $datastore ->entity($key,['user'=>$user->getEmail(),'station' =>$search_data]);
-$datastore->insert($task_favorite);
-echo "task compelete";
-} 
+$query = checkData($datastore,$search_data,$user);
+if($query==null){	
+	$datastore->insert($task_favorite);
+	echo "insert task compelete";
+}
+else {
+	echo "it is testing part";
+	echo "<pre>";
+	foreach ($query as $entity){
+	echo $entity['station'];
+	echo "<pre>";
+	}
+}
+}
+/*
+function: checkData
+@param: DatastoreClient datastore - the datastore we have created.
+		$search_data - the input data comes from search bar.
+		User $user - the object of logged in user
+@return: null = data not exsist.
+		 $result - the result of the query
+*/
+function checkData(DatastoreClient $datastore, $search_data,User $user)
+{
+	$query = $datastore->query()->kind('Favorite')->filter('user','=',$user->getEmail());
+	$result = $datastore->runQuery($query);
+	if(empty($result))
+	{
+		return null;
+	}
+	else
+		return $result;
+}
 ?>
